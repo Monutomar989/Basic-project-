9b26cf386c7ef2b9efa3412152dda761f17124de
monutomar989 <tomarmonu989@gmail.com>
2024-02-10 08:06:07 +0530

    sdfsdf

---
+++ Hello.py
@@ -0,0 +1,33 @@
+#Lets start.....
+
+print("Hello Everyone")
+
+# let suppose we have 2 variable these are given below
+
+a=int(input("Enter your First Value: "))
+b=int(input("Enter your Second Value: "))
+
+# # these 2 variable are dynamic which are depend on directly you thinking what you put here
+#
+# # so we start form mathematics rule
+# # 1. is addition
+# print("addition")
+# print(a+b)
+# # 2. in subtraction
+# print("subtraction")
+# print(a-b)
+# # 3. is multiplication
+# print("multiplication")
+# print(a*b)
+# # 4. is dividation(1)
+# print("dividation(1)")
+# print(a/b)
+# #  this dividation rule give floot value
+# # 5. dividation (2)
+# print("dividation (2)")
+# print(a//b)
+# # this rule give me int value
+# # 6. is Modules
+# print("modules")
+# print(a%b)
+
---
+++ Relation opeator.py
@@ -0,0 +1,45 @@
+#  Lets we start Types of operator
+#  1. Relation Operator
+# > -> greater than
+# < -> less than
+# == -> equal to
+# ! -> not equal to
+
+#  for example
+
+# # -> Greater then
+# a=int(input("Enter your First Number: "))
+# b=int(input("Enter your second number: "))
+# if a>b:
+#     print("A is Greater then B")
+# else:
+#     print("B is Greater then A")
+#
+# #  -> Less Then
+# a=int(input("Enter your First Number: "))
+# b=int(input("Enter your second number: "))
+# if a<b:
+#     print("A is less then B")
+# else:
+#     print("B is less then A")
+#
+# # -> Equal to
+# a=int(input("Enter your First Number: "))
+# b=int(input("Enter your second number: "))
+# if a==b:
+#     print("A is equal to B")
+# else:
+#     print("A does not equal to B")
+
+# -> not equal to
+a=int(input("Enter your First Number: "))
+b=int(input("Enter your second number: "))
+if a!=b:
+    print("ohh no these are not same")
+else:
+    print("ohh shit these are really same")
+
+
+
+
+
